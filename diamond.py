9dd3b99b69c62e8b6a73f68b74065e459615dc22
Myouuu <meriem.mrabent@hetic.net>
2021-07-13 12:07:28 +0200

    :snake: implementation in python

---
+++ diamond.py
@@ -0,0 +1,10 @@
+def diamond():
+
+    num = 9
+
+    for i in range(1, num+1):
+      i = i - (num//2 +1)
+      if i < 0:
+        i = -i
+      print(" " * i + "*" * (num - i*2) + " "*i)
+diamond()
